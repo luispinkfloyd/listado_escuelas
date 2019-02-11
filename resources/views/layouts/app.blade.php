b3259f136674f1d7b50ed852687644083f33b516
luispinkfloyd <luisrodriguez@untref.edu.ar>
2019-02-11 11:47:08 -0300

    Modificado el navbar para que muestre el título de la aplicación con el botón toggle

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -104,9 +104,6 @@
                 <a class="navbar-brand" href="{{ url('/') }}">
                 	<img src="{{ asset('img/untref.png')}}" height="50" class="d-inline-block align-top" style="padding-right:10px">
                 </a>
-                <a class="navbar-brand" href="{{ url('/') }}" style="color:#FFFFFF">
-                	<h3><b>Listado de Escuelas<br>CABA Conurbano</b></h3>
-                </a>
                 <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
                     <span class="navbar-toggler-icon"></span>
                 </button>
@@ -114,7 +111,9 @@
                 <div class="collapse navbar-collapse" id="navbarSupportedContent">
                     <!-- Left Side Of Navbar -->
                     <ul class="navbar-nav mr-auto">
-
+						<a class="navbar-brand" href="{{ url('/') }}" style="color:#FFFFFF">
+                            <h3><b>Listado de Escuelas<br>CABA Conurbano</b></h3>
+                        </a>
                     </ul>
 
                     <!-- Right Side Of Navbar -->
